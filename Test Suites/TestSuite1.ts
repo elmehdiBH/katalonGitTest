07da1408a52d1673797601d569f6515747b05fea
elmehdiBH <81761688+elmehdiBH@users.noreply.github.com>
2021-05-02 22:32:51 +0200

    test message

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>533c1533-5844-40a7-a21f-a05969752a81</testSuiteGuid>
+</TestSuiteEntity>
